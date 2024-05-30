dff1419bc8e9f6b10e1403edc7b0585ad2d415a7
MateoAlvarez-Dev <77896899+MateoAlvarez-Dev@users.noreply.github.com>
2024-05-30 15:43:10 -0500

    Files chat.routes and chat.handlebars created

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/chat", (req, res) => {
-    res.send("OK");
+    res.render("main", { layout: 'home', products: JSON.parse(JSON.stringify(products)) });
 });
 
 module.exports = router;
